a3faafcfc91fb6be0c070345a49469bf743e0d2f
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2023-07-08 16:42:56 +0000

    Initial commit

---
+++ handler/variables.js
@@ -0,0 +1,6 @@
+/*Store all the variables here*/
+module.exports = (bot) => {
+  bot.variables({
+    author: "Eddy"
+  });
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+const BOT_TOKEN = process.env['BOT_TOKEN'];
+const { AoiClient, LoadCommands } = require("aoi.js");
+
+const bot = new AoiClient({
+    token: BOT_TOKEN,
+    prefix: "!",
+    intents: ["MessageContent", "Guilds", "GuildMessages"],
+    events: ["onMessage"],
+    database: {
+        type: "aoi.db",
+        db: require("@akarui/aoi.db"),
+        tables: ["main"],
+        path: "./database/",
+        extraOptions: {
+            dbType: "KeyValue"
+        }
+    }
+});
+
+require('./handler/variables.js')(bot);
+
+const loader = new LoadCommands(bot);
+loader.load(bot.cmd, "./commands/");
